22866c2be76e435b50006e9969bb573eb7a352ba
David <meyer.david221@gmail.com>
2019-11-27 00:09:40 -0700

    Initial push of folders and MatLab Script

---
+++ David_Meyer_FinalProject.m
@@ -0,0 +1,24 @@
+%David Meyer Final Project:Skin Cancer
+%CS390S
+%11/26/2019 to 12/0#/2019
+
+%Can't work on all skin cancer, so work on just one small branch to start
+%and then work on the next one...
+
+
+
+%Decided to start working on just Melnoma skin cancer because it has a lot
+%of different visual ques to show that it needs to be tested by a doctor.
+%Marking this, even with a 100%, with this program only shows that it is
+%"most likely" not a gaurantee to be malignant(Cancerous).
+
+
+
+%% RESEARCH AREA
+
+
+
+%% Programing
+clc
+clear all
+close all
